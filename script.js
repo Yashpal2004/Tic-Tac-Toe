9bb79899ea6d308c5d2db11160a96888657cd8cf
Yashpal2004 <meyashpal2004@gmail.com>
2024-02-09 11:51:43 +0530

    turns

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 window.onload = function() {
   const boxs = ["tl", "tc", "tr", "cl", "cc", "cr", "bl", "bc", "br"]
   
+  const win = [
+    ["tl", "tc", "tr"],
+    ["cl", "cc", "cr"],
+    ["bl", "bc", "br"],
+    ["tl", "cl", "bl"],
+    ["tc", "cc", "bc"],
+    ["tr", "cr", "br"],
+    ["tl", "cc", "br"],
+    ["bl", "cc", "bl"]
+  ]
+
   let turn = 1
 
   for (const i in boxs) {
@@ -20,4 +31,14 @@ window.onload = function() {
     }
     turn = turn + 1
   }
+
+  const player = {
+    turn1:"turn1",
+    turn2:"turn2",
+    turn3:"turn3",
+    turn4:"turn4",
+    turn5:"turn5"
+  }
+
+
 }
